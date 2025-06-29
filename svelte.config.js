af5108e5c633c27bee8ac270592d04ac45188fbd
Mattheus Faria <mfaria@wpi.edu>
2025-06-29 19:00:57 -0400

    Please

--- svelte.config.js
+++ svelte.config.js
@@ -6,17 +6,17 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	prerender : {
-		entries: [
-			'*',
-		]
-	},
 
 	kit: {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			fallback: 'index.html'
+		}),
+		paths: {
+			base: ''
+		}
 	}
 };
 
